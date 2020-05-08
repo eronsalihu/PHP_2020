79c48fd8c72d9d58fe50f6ef97817037bc3c78a7
Enis Berisha <58141985+enisbe1@users.noreply.github.com>
2020-05-09 00:20:23 +0200

    Create dbconfig.php

---
+++ Programimi_ne_Internet/dbconfig.php
@@ -0,0 +1,16 @@
+<?php
+
+$db_user="root";
+$db_pass="E.berisha1";
+$db_name="smartapp";
+$message="";
+try {
+  $db=new PDO('mysql:host=localhost;dbname=' . $db_name .';charset=utf8;',$db_user,$db_pass);
+  session_start();
+  $db->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+
+} catch (PDOEXCEPTION $e) {
+$message=$e->getMessage();
+}
+
+?>
