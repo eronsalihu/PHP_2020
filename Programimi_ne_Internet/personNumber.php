8120446945999c03eeb18cad56042f949792ae36
endritbalaj1 <63596116+endritbalaj1@users.noreply.github.com>
2020-05-13 00:17:52 +0200

    Add files via upload

---
+++ Programimi_ne_Internet/personNumber.php
@@ -0,0 +1,13 @@
+<?php
+
+if(isset($_SESSION['page_count']))
+{
+     $_SESSION['page_count'] += 1;
+}
+else
+{
+     $_SESSION['page_count'] = 1;
+}
+ echo 'You are visitor number ' . $_SESSION['page_count'];
+ 
+?>
