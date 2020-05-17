a580428aed3add4757c6343bf85d665bd2052ba0
endritbalaj1 <63596116+endritbalaj1@users.noreply.github.com>
2020-05-17 14:25:01 +0200

    Update inheritance.php

--- Programimi_ne_Internet/inheritance.php
+++ Programimi_ne_Internet/inheritance.php
@@ -1,49 +1,56 @@
 <?php
-    class employee {
-        protected $fname;
-        protected $lname;
+    class studenti {
+        protected $emri;
+        protected $mbiemri;
 
-        public function __construct($firstName,$lastName)
+        public function __construct($emriJuaj,$mbiemriJuaj)
         {
-            $this->fname = $firstName;
-            $this->lname = $lastName;
+            $this->emri = $emriJuaj;
+            $this->mbiemri = $mbiemriJuaj;
         }
-        public function getFullName()
+        public function EmriPlote()
         {
-            echo "<br>Full Name: ". $this->fname." ". $this->lname;
+            echo "<br>Full Name: ". $this->emri." ". $this->mbiemri;
         }
-
+     
     }
 
 
-    class dept extends employee{
-        private $deptNo;
-        private $deptName;
-        public function __construct($firstName,$lastName,$deptNumber,$departName)
+    class departament extends studenti{
+        private $numriDepartamentit;
+        private $emriDepartamentit;
+        public function __construct($emriJuaj,$mbiemriJuaj,$nrOfDepartment,$nameOfDepartment)
         {
-            parent::__construct($firstName,$lastName);
-            $this->deptNo = $deptNumber;
-            $this->deptName = $departName;
+            parent::__construct($emriJuaj,$mbiemriJuaj);
+            $this->numriDepartamentit = $nrOfDepartment;
+            $this->emriDepartamentit = $nameOfDepartment;
         }
-        public function setDeptNo($deptNo){
-         $this->deptNo = $deptNo;
-      }
-      public function getDeptNo(){
-         return  $this->deptNo;
-      }
-      public function setDeptName($deptName){
-         $this->deptName = $deptName;
-      }
-      public function getDeptName(){
-         return  $this->deptName;
-      }
+        public function setDeptNo($numriDepartamentit){
+         $this->numriDepartamentit = $numriDepartamentit;
+        }
+        public function getDeptNo(){
+            return  $this->numriDepartamentit;
+        }
+        public function setDeptName($emriDepartamentit){
+            $this->emriDepartamentit = $emriDepartamentit;
+        }
+        public function getDeptName(){
+            return  $this->emriDepartamentit;
+        }
         public function getDept()
         {
-            echo "<br> Dept No:".$this->deptNo."   Dept Name: ".$this->deptName;
+            echo "<br> Dept No:".$this->numriDepartamentit."   Dept Name: ".$this->emriDepartamentit;
         }
+        public function __destruct() 
+        { 
+            echo "\t2020"; 
+        }   
 
     }
-    $objDept = new dept("Computer", "Engineering", 5, "FIEK");
-    $objDept->getFullName();
+
+    $objDept = new departament("Computer", "Engineering", 5, "FIEK");
+    $objDept->EmriPlote();
     $objDept->getDept();
+    $objDept->__destruct();
+    
 ?>
